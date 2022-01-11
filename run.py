1bb4fac6f48a0cfbdda21b8129e0e8a31fd2ae28
fergalquinn77 <fergal@bourbon.ie>
2022-01-11 19:55:34 +0000

    Initial commit - prompt for grid size and setup user and computer board

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+game_size=int(input("Enter grid size (8-15):"))
+"""
+Get game size - give option of 8-15 rows
+"""
+my_board=[[' '*game_size for x in range(game_size)]]
+computer_board=[[' '*game_size for x in range(game_size)]]
+print(my_board)
+
