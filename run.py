2475b4366ae344d6f774f1b08671b30cf68d8e1a
fergalquinn77 <fergal@bourbon.ie>
2022-01-11 20:22:07 +0000

    Setup structure of forseen functions

--- run.py
+++ run.py
@@ -3,7 +3,32 @@ game_size=int(input("Enter grid size (8-15):"))
 """
 Get game size - give option of 8-15 rows
 """
-my_board=[[' '*game_size for x in range(game_size)]]
-computer_board=[[' '*game_size for x in range(game_size)]]
-print(my_board)
+user_board = [[' '*game_size for x in range(game_size)]]
+computer_board = [[' '*game_size for x in range(game_size)]]
+
+def user_ship_locations():
+    for i in range(game_size):
+        print(f"Enter co-orderinates of ship number {i}")
+        input("Enter x,y co-ordinates of ship ")
+
+def check_grid_input():
+    pass
+
+def check_ship_input():
+    pass
+
+def computer_ship_locations():
+    pass
+
+def user_move():
+    pass
+
+def adjust_scores():
+    pass
+
+def
+
+
+
+
 
