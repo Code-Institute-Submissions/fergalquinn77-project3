987dd01931481881e48c4201aef61c7ad10d46ed
fergalquinn77 <fergal@bourbon.ie>
2022-02-12 15:36:52 +0000

    Test

--- run.py
+++ run.py
@@ -1,56 +1,20 @@
+import random
+import gspread
+from google.oauth2.service_account import Credentials
 
-game_size=0
-def get_grid_size():
-    global game_size
-    while True:
-        game_size=int(input("Enter grid size (8-15):"))
-        if check_grid_input(game_size):
-            print("Data is valid!")
-            break
-    return game_size
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-"""
-Get game size - give option of 8-15 rows
-"""
-user_board = [[' '*game_size for x in range(game_size)]]
-computer_board = [[' '*game_size for x in range(game_size)]]
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("battleships")
 
-def user_ship_locations():
-    for i in range(5):
-        print(f"Enter co-orderinates of ship number {i+1}")
-        co_ord=input("Enter x,y co-ordinates of ship separated by a comma")
-        
+test
 
-def check_grid_input(data):
-    
-    try:
-        if data <8 or data >15:
-            raise ValueError(
-                f"Value needs to be between 8 & 15"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-        return False
-    return data
-    
-
-def check_ship_input():
-    pass
-
-def computer_ship_locations():
-    pass
-
-def user_move():
-    pass
-
-def adjust_scores():
-    pass
-
-def main():
-    get_grid_size()
-    check_grid_input(game_size)
-
-main()
-
+test 2
 
 
